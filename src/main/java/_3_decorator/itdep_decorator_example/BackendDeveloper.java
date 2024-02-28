f573f9cae8d6609ee3cafe7fa2e76a0c16f3f14f
admin <amalsaidhanov1031@gmail.com>
2024-02-28 12:54:37 +0800

    FileInputStream Decorator (LowerCaseInSt)

--- src/main/java/_3_decorator/itdep_decorator_example/BackendDeveloper.java
+++
@@ -1,8 +0,0 @@
-package _3_decorator.itdep_decorator_example;
-
-public class BackendDeveloper implements ITDepartment{
-    @Override
-    public String makeWebsite() {
-        return "BACKEND";
-    }
-}
--- src/main/java/_3_decorator/itdep_decorator_example/ITDepartment.java
+++
@@ -1,5 +0,0 @@
-package _3_decorator.itdep_decorator_example;
-
-public interface ITDepartment {
-    String makeWebsite();
-}
